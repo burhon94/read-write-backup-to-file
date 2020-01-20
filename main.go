b9692ad84a79bfebee0d7cf6681a1fb3a3c52ad6
BT <BT@mail.ru>
2020-01-20 18:05:47 +0500

    We check the file, if the file is not exist, we create it and then write the data, and if the file is exist, then we rewrite its data with new data

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -14,11 +15,10 @@ type income struct {
 	amount uint64
 }
 
-var data = []income{
+var datai = []income{
 	{1, 1_000_000},
 	{2, 2_000_000},
 	{3, 3_000_000},
-	{4, 4_000_000},
 }
 
 var customError = errors.New("custom error")
@@ -27,37 +27,58 @@ func makeError() error {
 	return fmt.Errorf("error generated: %w", customError)
 }
 
-var dataBytes = [] byte{}
+//var dataBytes = [] byte{}
 
 ////////////////////////////CONVERT-TO-WRITE
 func dataStructToBytes(data []income) (dataBytes []byte) {
 	log.Print("conversion this struct data is started")
-	log.Println("\n", data)
+	log.Print(data)
 	dataStrings := make([]string, len(data))
 	for i, datum := range data {
 		dataStrings[i] = fmt.Sprintf("id:%d;amount:%d", datum.id, datum.amount)
 	}
 	oneString := strings.Join(dataStrings, "\n")
 	dataBytes = []byte(oneString)
-	log.Print("data converted to slice[] byte")
+	log.Println("data converted to slice[] byte\n", dataBytes)
 	return
 }
 
 ////////////////////////////WRITE-TO-FILE
 func Write(data []byte, path string) string {
-	log.Print("data saving started")
-	err := ioutil.WriteFile(path, data, 0666)
+	var _, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		log.Print("file \"", path, "\" not exist for write")
+		log.Print("file \"", path, "\" will be create")
+		var file, err = os.Create(path)
+		if err != nil {
+			return "file could not be created successfully"
+		}
+		defer file.Close()
+		log.Print("file \"", path, "\" created successfully")
+		log.Print("this data will saved")
+		log.Print(data)
+		err = ioutil.WriteFile(path, data, 0666)
+		if err != nil {
+			log.Fatal(fmt.Errorf("can't save to %s: %w", path, err))
+		}
+		log.Println("data saved")
+		return ""
+	}
+	log.Print("file ", path, " is exist")
+	//TODO: сделать резервную копию файла
+	//log.Fatal("WEE NEED TODO BACKUP A FILE AND AFTER WRITING DATA TO A FILE")
+	log.Print("this data will saved")
+	log.Print(data)
+	err = ioutil.WriteFile(path, data, 0666)
 	if err != nil {
 		log.Fatal(fmt.Errorf("can't save to %s: %w", path, err))
 	}
-	log.Print("this data will saved")
-	log.Println(data)
-	log.Print("data saved")
+	log.Println("data saved")
 	return ""
 }
 
 ////////////////////////////CONVERT-TO-STRUCT
-func bytesToDataStruct(bytes string) (data []income) {
+func StringToDataStruct(bytes string) (data []income) {
 	dataString := string(bytes) //Converting Slice_Byte_Type to String_Type
 	log.Print("data loaded")
 	log.Print("conversion this data is started")
@@ -88,7 +109,7 @@ func bytesToDataStruct(bytes string) (data []income) {
 		}
 		data = append(data, element)
 	}
-	log.Print("data converted to struct")
+	log.Println("data converted to struct")
 	return
 }
 
@@ -99,21 +120,18 @@ func readFromFile(path string) (string, error) {
 	if err != nil {
 		log.Fatal(fmt.Errorf("can't read from %v: %w", path, err))
 	}
-	log.Print("file \"", path, "\" was opened")
+	log.Println("file \"", path, "\" was opened")
 	return string(bytes), nil
 }
 
 func main() {
-
-	data3 := dataStructToBytes(data)//data3 -> converted []byte
-	fmt.Println(data3)
-	Write(data3, "data2.txt")//data3 -> write to file data2.txt
-	//fmt.Println(data3)
-	valueToRead, err := readFromFile("data2.txt")
+	data3 := dataStructToBytes(datai) //data3 -> converted []byte
+	Write(data3, "data2.txt")         //data3 -> write to file data2.txt if not exist create data2.txt
+	valueToRead, err := readFromFile("data2.txt") //read from data2.txt
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(valueToRead)
-	valueToStruct := bytesToDataStruct(valueToRead)
-	fmt.Print(valueToStruct)
+	fmt.Println(valueToRead) //show data from data2.txt
+	valueToStruct := StringToDataStruct(valueToRead) //data from data2.txt converted to struct []income
+	fmt.Println(valueToStruct) //show struct []income after convert
 }
