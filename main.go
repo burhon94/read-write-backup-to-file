685b461b57679e3d0e1d8ab4d1edc82a2cc1c288
BT <BT@mail.ru>
2020-01-20 20:57:27 +0500

    We implemented  backup function the file

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type income struct {
@@ -17,8 +19,10 @@ type income struct {
 
 var datai = []income{
 	{1, 1_000_000},
-	{2, 2_000_000},
+	//{2, 2_000_000},
 	{3, 3_000_000},
+	{4, 4_000_000},
+	//{5, 5_000_000},
 }
 
 var customError = errors.New("custom error")
@@ -45,8 +49,8 @@ func dataStructToBytes(data []income) (dataBytes []byte) {
 
 ////////////////////////////WRITE-TO-FILE
 func Write(data []byte, path string) string {
-	var _, err = os.Stat(path)
-	if os.IsNotExist(err) {
+	var _, err1 = os.Stat(path)
+	if os.IsNotExist(err1) {
 		log.Print("file \"", path, "\" not exist for write")
 		log.Print("file \"", path, "\" will be create")
 		var file, err = os.Create(path)
@@ -65,8 +69,27 @@ func Write(data []byte, path string) string {
 		return ""
 	}
 	log.Print("file ", path, " is exist")
-	//TODO: сделать резервную копию файла
-	//log.Fatal("WEE NEED TODO BACKUP A FILE AND AFTER WRITING DATA TO A FILE")
+	var srcFile, err2 = os.Open(path)
+	if err2 != nil {
+		log.Fatal(fmt.Errorf("can't check source file %s: %w", path, err2))
+	}
+	log.Print("source file was checked successfully")
+	dateTimeBackup := time.Now().Format("Backup(01-02-2006-15-04-5).txt")
+	log.Print("copy date and time for copy")
+	fmt.Println(dateTimeBackup)
+	log.Print("try create file to backup")
+	var destFile, err = os.Create(dateTimeBackup)
+	if err != nil {
+		log.Fatal(fmt.Errorf("can't create file to backup %s: %w", path, err))
+	}
+	log.Print("backup file was create")
+	defer destFile.Close()
+	log.Print("try copy from file to backup file")
+	_, err = io.Copy(destFile, srcFile)
+	if err != nil {
+		log.Fatal(fmt.Errorf("can't finish copying %s: %w", path, err))
+	}
+	log.Print("file was copy to backup file")
 	log.Print("this data will saved")
 	log.Print(data)
 	err = ioutil.WriteFile(path, data, 0666)
@@ -125,13 +148,13 @@ func readFromFile(path string) (string, error) {
 }
 
 func main() {
-	data3 := dataStructToBytes(datai) //data3 -> converted []byte
-	Write(data3, "data2.txt")         //data3 -> write to file data2.txt if not exist create data2.txt
+	data3 := dataStructToBytes(datai)             //data3 -> converted []byte
+	Write(data3, "data2.txt")                     //data3 -> write to file data2.txt if not exist create data2.txt
 	valueToRead, err := readFromFile("data2.txt") //read from data2.txt
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(valueToRead) //show data from data2.txt
+	fmt.Println(valueToRead)                         //show data from data2.txt
 	valueToStruct := StringToDataStruct(valueToRead) //data from data2.txt converted to struct []income
-	fmt.Println(valueToStruct) //show struct []income after convert
+	fmt.Println(valueToStruct)                       //show struct []income after convert
 }
